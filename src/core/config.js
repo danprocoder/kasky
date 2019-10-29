3af42ff59174218094de5f5d63975987bc9814b1
Daniel Austin <danielaustin718@gmail.com>
2019-10-29 13:58:02 +0100

    Feature for a value in a json file or javascript object to refer to another value within itself

--- src/core/config.js
+++ src/core/config.js
@@ -1,48 +1,74 @@
 const path = require('path');
 const fs = require('fs');
 
-let config;
-exports.load = function() {
-  config = require(path.join(process.cwd(), 'app.config.json'));
-};
-
-function parseValues(config) {
-  
-
-  return config;
+function Parser(config) {
+  this.injectionSyntax = /\{([a-zA-Z\.]+)\}/;
+  this.baseConfig = config;
 }
 
-function getInjectableValue(p1) {
+Parser.prototype._parseValues = function(config) {
+  Object.keys(config).forEach(key => {
+    const value = config[key];
+
+    if (typeof value === 'string') {
+      config[key] = this._parseValueInjections(value);
+    } else if (typeof value === 'object') {
+      config[key] = this._parseValues(value);
+    }
+  });
+
+  return config;
+};
+
+Parser.prototype.parse = function() {
+  return this._parseValues(this.baseConfig);
+};
+
+/**
+ * @param {string} p1 The key to search for.
+ * 
+ * @returns {any|undefined} Returns the value for the searched key or undefined if the key was not found. 
+ */
+Parser.prototype._searchValue = function(p1) {
   const keys = p1.split('.');
-  let currentValue = config;
+  let currentValue = this.baseConfig;
 
   for (let i = 0; i < keys.length && typeof currentValue !== 'undefined'; i++) {
     currentValue = currentValue[keys[i]];
   }
-  return typeof currentValue === 'object' || currentValue instanceof Array
-    ? undefined // Didn't point to a valid value
-    : currentValue;
+  
+  return currentValue;
 }
 
-function parseValueInjections(value) {
-  const injectionSyntax = /\{([a-zA-Z\.]+)\}/;
-
-  while (value.match(injectionSyntax)) {
-    value = value
-        .replace(injectionSyntax, function(match, p1) {
-          const inject = getInjectableValue(p1);
-          return inject ? inject : match;
-        });
+Parser.prototype._parseValueInjections = function(value) {
+  let matchResult;
+  // Run as long as value contains injection syntax
+  while (matchResult = value.match(this.injectionSyntax)) {
+    const resolvedValue = this._searchValue(matchResult[1]);
+    
+    if (resolvedValue) {
+      if (value === matchResult[0] && typeof resolvedValue !== 'string') {
+        value = resolvedValue;
+        break;
+      } else {
+        value = value.replace(matchResult[0], resolvedValue);
+      }
+    } else {
+      break;
+    }
   }
   
   return value;
 }
+let config;
 
-exports.get = function(key) {
-  const value = config[key];
-  return typeof value === 'string' ? parseValueInjections(config[key]) : value;
-}
+exports.load = function() {
+  config = require(path.join(process.cwd(), 'app.config.json'));
+  config = new Parser(config).parse();
+};
+
+exports.get = key => config[key];
 
 if (process.env.NODE_ENV === 'test') {
-  exports.parseValues = parseValues;
+  exports.Parser = Parser;
 }
--- test/core/config.js
+++ test/core/config.js
@@ -4,7 +4,16 @@ const config = require('../../src/core/config');
 describe('Test src/core/config.js', () => {
   
   it('it should inject all values', () => {
-    const parsed = config.parseValues({
+    const parsed = new config.Parser({
+      project: {
+        config: {
+          rootDir: '{rootDir}',
+          somePath: '{somePath}'
+        },
+        developer: {
+          drinks: '{tea.name} ({tea.type})'
+        }
+      },
       tea: {
         name: '{var.name}',
         type: '{var.type} tea'
@@ -14,13 +23,22 @@ describe('Test src/core/config.js', () => {
         type: 'green'
       },
       rootDir: '/src',
-      somePath: '{rootDir}/somePath'
-    });
+      somePath: '{rootDir}/somePath',
+      someOtherPath: '{rootDir}{somePath}/end',
+      o: '{project.config}',
+      unknown: '{does.not.exists}'
+    }).parse();
 
+    expect(parsed.project.config.rootDir).to.be.eq('/src');
+    expect(parsed.project.config.somePath).to.be.eq('/src/somePath');
+    expect(parsed.project.developer.drinks).to.be.eq('lipton (green tea)');
     expect(parsed.tea.name).to.be.eq('lipton');
     expect(parsed.tea.type).to.be.eq('green tea');
     expect(parsed.rootDir).to.be.eq('/src');
     expect(parsed.somePath).to.be.eq('/src/somePath');
+    expect(parsed.someOtherPath).to.be.eq('/src/src/somePath/end');
+    expect(parsed.o).to.be.eq(parsed.project.config);
+    expect(parsed.unknown).to.be.eq('{does.not.exists}');
   });
 
 });
