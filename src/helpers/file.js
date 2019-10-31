0b52c758a106578382cde031600532397c508514
Daniel Austin <danielaustin718@gmail.com>
2019-10-31 17:18:47 +0100

    Write base compiler class

--- src/helpers/file.js
+++ src/helpers/file.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const glob = require('glob')
 
 /**
  * Deletes a directory with all it's content inside recursively.
@@ -26,4 +27,20 @@ function deleteDir (dir) {
   fs.rmdirSync(dir)
 }
 
-exports.deleteDir = deleteDir
+function matches (pattern, callback) {
+  glob(pattern, null, (err, matches) => {
+    if (err) throw new Error(err)
+
+    callback(matches)
+  })
+}
+
+function readString (filepath) {
+  return fs.readFileSync(filepath, { encoding: 'utf-8' })
+}
+
+module.exports = {
+  deleteDir,
+  matches,
+  readString
+}
